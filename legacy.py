49acda33a7554bb6d98cd26799830e25010f0c8b
UtkarshRa242 <52875649+UtkarshRa242@users.noreply.github.com>
2020-02-03 01:05:42 +0530

    Create legacy.py

---
+++ legacy.py
@@ -0,0 +1,3 @@
+print(" Hi Guys, I just wanted to say that this should exist till the end of our family. I have created it just for you. God bless everyone. Be courageous and never lose hope.") 
+print("UTKARSH RASTOGI ONE OF THE MOST BADASS ANCESTOR")
+print("created for python")
